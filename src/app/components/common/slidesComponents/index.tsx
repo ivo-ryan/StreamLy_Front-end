1f94c30263e722437d77f10f975a68d76b789a34
ivo-ryan <ivoryanalfredobastos@gmail.com>
2025-08-29 23:06:13 -0300

    Update

--- src/app/components/common/slidesComponents/index.tsx
+++ src/app/components/common/slidesComponents/index.tsx
@@ -1,41 +1,66 @@
 "use client"
 
+import styles from "./styles.module.scss";
 import { SeriesType } from "@/services/seriesServices";
-import { Swiper , SwiperSlide } from "swiper/react";
 
 import SlideCard from "../slideCard";
-import { Autoplay, Pagination } from "swiper/modules";
-import "swiper/css"
-import "swiper/css/autoplay"
-import "swiper/css/pagination"
+import { useKeenSlider } from "keen-slider/react"
+import "keen-slider/keen-slider.min.css"
+import { useState } from "react";
+
 
 interface props {
     series: SeriesType[];
 }
 
 export default function SlideComponent( {series}: props ){
+      const [currentSlide, setCurrentSlide] = useState(0);
+
+  const [sliderRef, instanceRef] = useKeenSlider<HTMLDivElement>({
+    loop: true,
+    slides: { perView: 4, spacing: 10 },
+    slideChanged(slider) {
+      setCurrentSlide(slider.track.details.rel);
+    },
+     breakpoints: {
+      "(min-width: 640px)": {
+        slides: { perView: 2, spacing: 15 },
+      },
+      "(min-width: 1024px)": {
+        slides: { perView: 4, spacing: 20 },
+      },
+
+      "(max-width: 500px)": {
+        slides: { perView: 1 }
+      }
+    }
+  });
+
     return (
         <>
-            <div style={{width: "90%"}}>
-                <Swiper spaceBetween={5}
-                        slidesPerView={"auto"} 
-                        loop={true}  
-                        autoplay={{ delay: 2000, disableOnInteraction: false }}
-                        modules={[Autoplay, Pagination]}
-                        pagination={{ clickable: true }}
-                        effect="fade"
-                        speed={2000}
-                        >
+            <div className={styles.container}>
+                <div ref={sliderRef} className="keen-slider">
+                    
                   
                 {
                     series?.map(item => (
-                        <SwiperSlide key={item.id}  style={{width: "280px"}}>
+                        <div key={item.id} className="keen-slider__slide " >
                             <SlideCard series={item}/>
-                        </SwiperSlide>
+                        </div>
                     ))
                 }
-
-                </Swiper>
+                </div>
+                 <div className="flex justify-center mt-4 gap-2">
+        {series.map((_, idx) => (
+          <button
+            key={idx}
+            className={`w-3 h-3 rounded-full ${
+              currentSlide === idx ? "bg-blue-500" : "bg-gray-300"
+            }`}
+            onClick={() => instanceRef.current?.moveToIdx(idx)}
+          />
+        ))}
+      </div>
             </div>
         </>
     )
