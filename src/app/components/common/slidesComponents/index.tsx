d33948c40a073dfd131e762d64092c8a490f91a0
ivo-ryan <ivoryanalfredobastos@gmail.com>
2025-09-01 22:57:36 -0300

    Created section slides

--- src/app/components/common/slidesComponents/index.tsx
+++ src/app/components/common/slidesComponents/index.tsx
@@ -4,9 +4,8 @@ import styles from "./styles.module.scss";
 import { SeriesType } from "@/services/seriesServices";
 
 import SlideCard from "../slideCard";
-import { useKeenSlider } from "keen-slider/react"
-import "keen-slider/keen-slider.min.css"
-import { useState } from "react";
+import { Splide, SplideSlide } from "splide-nextjs/react-splide";
+import 'splide-nextjs/splide/dist/css/themes/splide-default.min.css';
 
 
 interface props {
@@ -14,75 +13,52 @@ interface props {
 }
 
 export default function SlideComponent( {series}: props ){
-      const [currentSlide, setCurrentSlide] = useState(0);
-      const [loaded, setLoaded] = useState(false);
+  let slideCount = 0;
 
- const [sliderRef, instanceRef] = useKeenSlider<HTMLDivElement>({
-    loop: true,
-    created() {
-      setLoaded(true);
-    },
-  
-    slides: { perView: 4, spacing: 10 , origin: "center"},
-    mode: "snap",
-    slideChanged(slider) {
-      setCurrentSlide(slider.track.details.rel);
-    },
-     breakpoints: {
-      "(max-width: 800px)": {
-        slides: { perView: 2, spacing: 5 },
-      },
-      "(max-width: 1024px)": {
-        slides: { perView: 3, spacing: 8 },
-      },
-
-      "(max-width: 500px)": {
-        slides: { perView: 1 }
-      }
-    }
-  });
-
+  if (series.length > 4) {
+    slideCount = 4
+  }else if(series){
+    slideCount = series.length;
+  }
+ 
     return (
         <>
             <div className={styles.container}>
-                <div ref={sliderRef} className="keen-slider " style={{width: "93%"}}>
-                    
-                  
-                {
-                    series?.map(item => (
-                        <div key={item.id} className="keen-slider__slide " >
-                            <SlideCard series={item}/>
-                        </div>
-                    ))
-                }
+              		<div className="d-flex flex-column align-items-center justify-content-center py-4">
+                      <Splide
+                        options={{
+                          type: "loop",
+                          perPage: slideCount,
+                          perMove: 1,
+                          width: slideCount * 300,
+                          pagination: false,
+                          arrows: series.length > 4 ? true : false,
+                          drag: series.length > 4 ? true : false,
+                          breakpoints: {
+                            1200: {
+                              perPage: slideCount >= 2 ? 2 : 1,
+                              width: slideCount >= 2 ? 600 : 300,
+                              arrows: series.length > 2 ? true : false,
+                              drag: series.length > 2 ? true : false
+                            },
+                            600: {
+                              perPage: 1,
+                              width: 300,
+                              arrows: series.length > 1 ? true : false,
+                              drag: series.length > 1 ? true : false,
+                            }
+                          }
+                        }}
+                       
+                      >
+                        {series?.map((serie) => (
+                          <SplideSlide key={serie.id}>
+                            <SlideCard series={serie} />
+                          </SplideSlide>
+                        ))}
+                      </Splide>
                 </div>
-                {loaded && instanceRef.current && (
-        <div className={styles.arrowContainer}>
-          <button
-            onClick={() => instanceRef.current?.prev()}
-            className={styles.arrowLeft}
-          >
-            ←
-          </button>
-          <button
-            onClick={() => instanceRef.current?.next()}
-            className={styles.arrowRight} >
-            →
-          </button>
-        </div>
-      )}
 
-                 <div className={styles.containerBullets}>
-                      {series.map((_, idx) => (
-                          <span
-                            key={idx}
-                            className={`${styles.bullets} ${
-                              currentSlide === idx ? styles.bulletCur : styles.bulletNext
-                            }`}
-                            onClick={() => instanceRef.current?.moveToIdx(idx)}
-                          />
-                      ))}
-                  </div>
             </div>
         </>
     )
